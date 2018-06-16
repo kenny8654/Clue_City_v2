2a20dd4dbace7c04fccd03a0a019547e9f1966d7
Han <bigsamtw@gmail.com>
2018-06-16 15:13:08 +0800

    quick commit

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -1,27 +1,27 @@
-var current = { lat: 22.996950, lng: 120.222417 }   
+var current = { lat: 22.996950, lng: 120.222417 }
 var flag = 0;
 var time = 1800;
-var markersList = [];    
+var markersList = [];
 
-function addMarker(){
+function addMarker() {
   console.log("mark")
-    var marker = new google.maps.Marker({
-    position:  current,
+  var marker = new google.maps.Marker({
+    position: current,
     map: map,
     icon: './images/Map/marker.png'
   });
   markersList.push(marker);
 
-  
+
 }
 
 function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
+  map = new google.maps.Map(document.getElementById('map'), {
     zoom: 17,
     center: current,
     mapTypeId: 'terrain'
   });
-   addMarker()
+  addMarker()
 
   var script = document.createElement('script');
 
@@ -51,11 +51,11 @@ function eqfeed_callback(results) {
   map.data.addGeoJson(results);
 }
 
-gps = function(){
+gps = function () {
   navigator.geolocation.getCurrentPosition(successCallback,
     errorCallback,
     { maximumAge: 1000, timeout: 0 });
-    setTimeout(gps,2000)
+  setTimeout(gps, 2000)
 }
 gps()
 
@@ -63,21 +63,21 @@ function successCallback(position) {
   var crd = position.coords;
   current.lat = crd.latitude;
   current.lng = crd.longitude;
-  console.log('Latitude : ' + current.lat +'Longitude: ' + current.lng);
+  console.log('Latitude : ' + current.lat + 'Longitude: ' + current.lng);
 
-  if (flag == 0){
+  if (flag == 0) {
     console.log("init")
     initMap()
     flag = 1
-  }  
+  }
   function clearMarkers(addMarker) {
     console.log("delete")
-    for(var i = 0; i < markersList.length; i++) {
+    for (var i = 0; i < markersList.length; i++) {
       markersList[i].setMap(null);
-   }
-   addMarker()
+    }
+    addMarker()
   }
-  clearMarkers(addMarker) 
+  clearMarkers(addMarker)
 }
 
 function errorCallback(error) {
@@ -92,21 +92,19 @@ function errorCallback(error) {
 function doFallback() {
 }
 
-function Time()
-{
-    time -= 1;
-    if(Math.floor(time % 60) < 10)
-      document.getElementById('time').innerHTML= Math.floor(time / 60) +":0" +  Math.floor(time % 60);
-    else
-      document.getElementById('time').innerHTML= Math.floor(time / 60) +":" +  Math.floor(time % 60);
-    if(time!=0)
-    {
-        setTimeout("Time()",1000);
-    }
+function Time() {
+  time -= 1;
+  if (Math.floor(time % 60) < 10)
+    document.getElementById('time').innerHTML = Math.floor(time / 60) + ":0" + Math.floor(time % 60);
+  else
+    document.getElementById('time').innerHTML = Math.floor(time / 60) + ":" + Math.floor(time % 60);
+  if (time != 0) {
+    setTimeout("Time()", 1000);
+  }
 }
 Time();
 
-function btn_hide_onclick(){
+function btn_hide_onclick() {
   console.log("click")
   document.getElementById('btn_clue').style.visibility = 'hidden'
   document.getElementById('big_clues_picture').style.visibility = 'hidden'
@@ -121,7 +119,7 @@ function btn_hide_onclick(){
   document.getElementById('success').style.visibility = 'hidden'
 }
 
-function btn_camera_onclick(){
+function btn_camera_onclick() {
   document.getElementById('btn_clue').style.visibility = 'visible'
   document.getElementById('big_clues_picture').style.visibility = 'visible'
   document.getElementById('big_clue_background').style.visibility = 'visible'
@@ -132,7 +130,7 @@ function btn_camera_onclick(){
   document.getElementById('submit_button').style.visibility = 'visible'
 }
 
-function small_clue_onclick(){
+function small_clue_onclick() {
   document.getElementById('btn_clue').style.visibility = 'visible'
   document.getElementById('big_clues_picture').style.visibility = 'visible'
   document.getElementById('big_clue_background').style.visibility = 'visible'
@@ -147,41 +145,67 @@ function small_clue_onclick(){
 $(document).ready(function () {
 });
 
-function onSubmitButtonClicked(){
+function onSubmitButtonClicked() {
   event.preventDefault();
 
   var formData = new FormData($('#upload_form')[0]);
   console.log('post');
   $.ajax({
-    url : '/upload',
-    type : 'post',
-    data : formData,
-    dataType : 'text',
-    success : function(data){
+    url: '/upload',
+    type: 'post',
+    data: formData,
+    dataType: 'text',
+    success: function (data) {
       //$('#upload_response').text(data);
       //console.log(data)
       console.log("-************************")
-      if(data == "similar"){
+      if (data == "similar") {
         document.getElementById('success').style.visibility = 'visible'
       }
-      else{
+      else {
         document.getElementById('try_again').style.visibility = 'visible'
       }
-    },  
-    error: function(){
+    },
+    error: function () {
       console.log("error!!!!!!!!!")
     },
-//    complete: function () {
-              //Handle the complete event
-//                       console.log("ajax completed ");
-//    }
     cache: false,
     contentType: false,
     processData: false,
   })
 }
 
-$('#submit_button').click(function(event){
-  event.preventDefault();    
-  $('#upload_form').submit();
-});
+function uploadFile() {
+  //上傳圖片的input
+  var file = document.getElementById("submit_image")
+  //因為準備用post提交，又因為圖片的內容比較大，所以我們選擇使用formdata來承載數據
+  //創建formdata對象
+  var formData = new FormData();
+  //給formdata對象中放入數據(鍵值對的方式)
+  formData.append('file', file.files[0]);
+  //提交請求
+  $.ajax({
+    url: '/upload',//請求路徑
+    type: 'POST',
+    data: formData,
+    contentType: false,//為了讓瀏覽器根據傳入的formdata來判斷contentType
+    processData: false,//同上
+    success: function (data) {
+      if (200 === data.code) {
+        console.log("上傳成功！");
+        if (data == "similar") {
+          document.getElementById('success').style.visibility = 'visible'
+        }
+        else {
+          document.getElementById('try_again').style.visibility = 'visible'
+        }
+      } else {
+        console.log("上傳失敗！");
+      }
+    },
+    error: function () {
+      console.log("與服務器通信發生錯誤");
+    }
+  });
+  console.log(1)
+}
--- routes/map.js
+++ routes/map.js
@@ -6,11 +6,11 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var stylus = require('stylus');
 const fs = require('fs');
-const http = require('http')
+const http = require('http');
 const querystring = require('querystring');
 const bodyParser = require('body-parser');
-const urlencoderParser = bodyParser.urlencoded({ extended: false })
-
+const urlencoderParser = bodyParser.urlencoded({ extended: false });
+const multer = require('multer')
 
 let mongoOperation = require("../../cluescity/mongo-express/main.js");
 let mongoCollection = "user";
@@ -26,73 +26,102 @@ router.post('/map', urlencoderParser,function(req,res){
  //     mongoOperation.mongoFind( mongoCollection , req.body);
 });
 
-router.post("/upload", urlencoderParser, function (req, res) {
-  req.setEncoding('binary');
-  var body = '';   // 文件数据
-  var fileName = '';  // 文件名
-  // 边界字符串
-  var boundary = req.headers['content-type'].split('; ')[1].replace('boundary=', '');
-  req.on('data', function (chunk) {
-    body += chunk;
-  });
+//配置diskStorage來控制文檔存儲的位置以及文檔名字等
+var storage = multer.diskStorage({
+  //確定圖片存儲的位置
+  destination: function (req, file, cb){
+      cb(null, './public/uploadImgs')
+  },
+  //確定圖片存儲時的名字,注意，如果使用原名，可能會造成再次上傳同一張圖片的時候的衝突
+  filename: function (req, file, cb){
+      cb(null, Date.now()+file.originalname)
+  }
+});
+//生成的專門處理上傳的一個工具，可以傳入storage、limits等配置
+var upload = multer({storage: storage});
 
-  req.on('end', function () {
-    var file = querystring.parse(body, '\r\n', ':')
+//接收上傳圖片請求的接口
+router.post('/upload', upload.single('file'), function (req, res, next) {
+  //圖片已經被放入到服務器裏,且req也已經被upload中間件給處理好了（加上了file等信息）
+  
+  //線上的也就是服務器中的圖片的絕對地址
+  var url = '/uploadImgs/' + req.file.filename
+  res.json({
+      code : 200,
+      data : url
+  })
+});
 
-    // 只处理图片文件
-    if (file['Content-Type'].indexOf("image") !== -1) {
-      //获取文件名
-      var fileInfo = file['Content-Disposition'].split('; ');
-      for (value in fileInfo) {
-        if (fileInfo[value].indexOf("filename=") != -1) {
-          //fileName = fileInfo[value].substring(10, fileInfo[value].length-1);
-          fileName = "./CluesCity/target.jpg"
-          if (fileName.indexOf('\\') != -1) {
-            //fileName = fileName.substring(fileName.lastIndexOf('\\')+1);
-            fileName = "./CluesCity/target.jpg"
-          }
-          console.log("File Name : " + fileName);
-        }
-      }
 
-      // 获取图片类型(如：image/gif 或 image/png))
-      var entireData = body.toString();
-      var contentTypeRegex = /Content-Type: image\/.*/;
 
-      contentType = file['Content-Type'].substring(1);
 
-      //获取文件二进制数据开始位置，即contentType的结尾
-      var upperBoundary = entireData.indexOf(contentType) + contentType.length;
-      var shorterData = entireData.substring(upperBoundary);
+// router.post("/upload", urlencoderParser, function (req, res) {
+//   req.setEncoding('binary');
+//   var body = '';   // 文件数据
+//   var fileName = '';  // 文件名
+//   // 边界字符串
+//   var boundary = req.headers['content-type'].split('; ')[1].replace('boundary=', '');
+//   req.on('data', function (chunk) {
+//     body += chunk;
+//   });
 
-      // 替换开始位置的空格
-      var binaryDataAlmost = shorterData.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
+//   req.on('end', function () {
+//     var file = querystring.parse(body, '\r\n', ':')
 
-      // 去除数据末尾的额外数据，即: "--"+ boundary + "--"
-      var binaryData = binaryDataAlmost.substring(0, binaryDataAlmost.indexOf('--' + boundary + '--'));
+//     // 只处理图片文件
+//     if (file['Content-Type'].indexOf("image") !== -1) {
+//       //获取文件名
+//       var fileInfo = file['Content-Disposition'].split('; ');
+//       for (value in fileInfo) {
+//         if (fileInfo[value].indexOf("filename=") != -1) {
+//           //fileName = fileInfo[value].substring(10, fileInfo[value].length-1);
+//           fileName = "./CluesCity/target.jpg"
+//           if (fileName.indexOf('\\') != -1) {
+//             //fileName = fileName.substring(fileName.lastIndexOf('\\')+1);
+//             fileName = "./CluesCity/target.jpg"
+//           }
+//           console.log("File Name : " + fileName);
+//         }
+//       }
 
-      // 保存文件
-      fs.writeFile(fileName, binaryData, 'binary', function (err) {
-        console.log(err);
-        // res.send('Image has been uploaded.');
-      });
-    } else {
-      // res.send('只能上传图片文件');
-    }
+//       // 获取图片类型(如：image/gif 或 image/png))
+//       var entireData = body.toString();
+//       var contentTypeRegex = /Content-Type: image\/.*/;
+
+//       contentType = file['Content-Type'].substring(1);
+
+//       //获取文件二进制数据开始位置，即contentType的结尾
+//       var upperBoundary = entireData.indexOf(contentType) + contentType.length;
+//       var shorterData = entireData.substring(upperBoundary);
+
+//       // 替换开始位置的空格
+//       var binaryDataAlmost = shorterData.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
+
+//       // 去除数据末尾的额外数据，即: "--"+ boundary + "--"
+//       var binaryData = binaryDataAlmost.substring(0, binaryDataAlmost.indexOf('--' + boundary + '--'));
+
+//       // 保存文件
+//       fs.writeFile(fileName, binaryData, 'binary', function (err) {
+//         console.log(err);
+//         // res.send('Image has been uploaded.');
+//       });
+//     } else {
+//       // res.send('只能上传图片文件');
+//     }
     
-    runPython(res);
+//     runPython(res);
 
-  })
-})
+//   })
+// })
 
-function runPython(res) {
-  console.log('Python is running')
-  var spawn = require("child_process").spawn;
-  var process = spawn('python3', ["./compare.py",]);
-  process.stdout.on('data', function (data) {
-    console.log(data.toString());
-    res.send(data.toString());
-  })
-}
+// function runPython(res) {
+//   console.log('Python is running')
+//   var spawn = require("child_process").spawn;
+//   var process = spawn('python3', ["./compare.py",]);
+//   process.stdout.on('data', function (data) {
+//     console.log(data.toString());
+//     res.send(data.toString());
+//   })
+// }
 
 module.exports = router;
