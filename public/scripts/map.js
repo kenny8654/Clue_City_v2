751aeb8db2a675325fed70ad40e355a3450aa15f
Han <bigsamtw@gmail.com>
2018-06-20 19:06:23 +0800

    1

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -409,8 +409,8 @@ function test() {
 
 function onSubmitButtonClicked() {
 
-  document.getElementById("processing").setStyle(" visibility: visible ")
-  var file = document.getElementById("submit_image")
+  document.getElementById("processing").style.visibility = "visible";
+  var file = document.getElementById("submit_image");
   //因為準備用post提交，又因為圖片的內容比較大，所以我們選擇使用formdata來承載數據
   //創建formdata對象
   var formData = new FormData();
