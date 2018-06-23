da7deda6828836f13712e253b952a562abd7d992
Han <bigsamtw@gmail.com>
2018-06-23 20:42:06 +0800

    1

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -669,7 +669,6 @@ function onSubmitButtonClicked() {
         console.log("not similar")
         success_or_failure = 0;
       }
-      fs.createReadStream('../../target.jpg').pipe(fs.createWriteStream('1.jpg'));
       setTimeout(function () {
         document.getElementById("success").style.visibility = "visible";
         document.getElementById("success_message").style.visibility = "visible";
