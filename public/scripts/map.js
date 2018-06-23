7fbf17c1fe4996a57a988a654db4b05417ef7c98
Han <bigsamtw@gmail.com>
2018-06-23 19:04:16 +0800

    1

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -5,8 +5,6 @@ var is_big_clue = 0;
 var markersList = [];
 var clue_index = 0;//系館
 var success_or_failure = 0;
-const fs = require('../../fs');
-
 
 function addMarker() {
   console.log("mark")
@@ -663,7 +661,6 @@ function onSubmitButtonClicked() {
         console.log("not similar")
         success_or_failure = 0;
       }
-      fs.createReadStream('../../target.jpg').pipe(fs.createWriteStream('1.jpg'));
       setTimeout(function () {
         document.getElementById("success").style.visibility = "visible";
         document.getElementById("success_message").style.visibility = "visible";
