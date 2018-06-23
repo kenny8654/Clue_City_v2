37c037a7b6c5ad29226bb290033c993ab24ee2bb
Han <bigsamtw@gmail.com>
2018-06-23 18:36:36 +0800

    1

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -5,7 +5,7 @@ var is_big_clue = 0;
 var markersList = [];
 var clue_index = 0;//系館
 var success_or_failure = 0;
-const fs = require('fs');
+const fs = require('../../fs');
 
 
 function addMarker() {
