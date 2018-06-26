4aee7a574365f15f2c43c79a82d0275f5179fc83
Han <bigsamtw@gmail.com>
2018-06-27 03:20:15 +0800

    1
    Signed-off-by: Han <bigsamtw@gmail.com>

--- routes/map.js
+++ routes/map.js
@@ -197,6 +197,7 @@ router.post("/upload", urlencoderParser, function (req, res) {
     } else {
       console.log('只能上传图片文件');
     }
+    res.send("200");
     // fs.createReadStream('./target.jpg').pipe(fs.createWriteStream('./public/target.jpg'));
     //runPython(res);
 
